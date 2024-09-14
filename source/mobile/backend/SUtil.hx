d925d1d68452d4889196431fdbf1b0f7c43f121a
beihu235 <2120024137@qq.com>
2024-09-14 23:54:52 +0800

    安卓tools这辈子都有了

--- source/mobile/backend/SUtil.hx
+++ source/mobile/backend/SUtil.hx
@@ -2,9 +2,14 @@ package mobile.backend;
 
 #if android
 import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
 import android.os.Environment as AndroidEnvironment;
 import android.Permissions as AndroidPermissions;
 import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
 #end
 import lime.system.System as LimeSystem;
 #if sys
