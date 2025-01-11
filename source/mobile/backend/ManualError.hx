845e5b2656f4f2e7fda660ed41951805978cbe37
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-01-11 16:26:44 +0800

    Add files via upload

---
+++ source/mobile/backend/ManualError.hx
@@ -0,0 +1,27 @@
+package mobile.backend;
+
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+#end
+
+using backend.CoolUtil;
+
+public static function saveErrorToFile(errorInfo:String):Void
+{
+    #if sys
+    try
+    {
+        if (!FileSystem.exists('crash'))
+            FileSystem.createDirectory('crash');
+
+        var fileName = 'crash/' + Date.now().toString().replace(' ', '-').replace(':', "'") + '.txt';
+        
+        File.saveContent(fileName, errorInfo);
+    }
+    catch (e:haxe.Exception)
+    {
+        trace('Couldn\'t save error message. (${e.message})');
+    }
+    #end
+}
