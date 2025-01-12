4e0b49978d0148a01e8b20d4d8762e82e91c7017
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-01-12 12:07:05 +0800

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -595,11 +595,14 @@ class FreeplayState extends MusicBeatState
 		try{
 			updateInfo(); //难度数据更新
 		} catch (e:Dynamic) {
-			openSubState(new ErrorSubState(e,e.stack));
+			mainerror = e;
 			ignoreCheck = true;
 			infoNote.data = 0;
 			infoRating.data = 0;
 			infoSpeed.data = 0; //搜索后无歌曲的数据更新
+		} catch (erro:haxe.Exception) {
+			excerror = erro.stack;
+			openSubState(new ErrorSubState(mainerror, excerror));
 		}
 		updateVoice();
 		_updateSongLastDifficulty();
