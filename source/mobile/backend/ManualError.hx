09990fb49405baf344b5634d9bad2d0a32a0337e
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-01-12 10:28:32 +0800

    Update ManualError.hx

--- source/mobile/backend/ManualError.hx
+++ source/mobile/backend/ManualError.hx
@@ -7,9 +7,26 @@ import sys.io.File;
 
 using backend.CoolUtil;
 
-function saveErrorToFile(errorInfo:String):Void
+function saveErrorToFile(main:String = null ,stack:String):Void
 {
     #if sys
+    var formattedStack = "";
+    var lines = stack.split("\n");
+    for (line in lines) {
+        if (line.startsWith("Called from file")) {
+            var parts = line.split(", ");
+            var fileLine = parts[0].split(" ");
+            var file = fileLine[2].replace(".hx", "");
+            var lineNum = fileLine[3];
+            var method = "";
+            if (parts.length > 1) {
+                var methodParts = parts[1].split(" ");
+                method = methodParts[1];
+            }
+            formattedStack += "$main\n$file/$method [line $lineNum]\n";
+        }
+    }
+    openSubState(new ErrorSubState(formattedStack))
     try
     {
         if (!FileSystem.exists('crash'))
