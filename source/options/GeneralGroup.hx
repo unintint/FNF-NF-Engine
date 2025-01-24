910d30a0238d38c0c89de1a0d71356f8c9c1db1c
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2025-01-24 19:38:08 +0800

    Add files via upload

---
+++ source/options/GeneralGroup.hx
@@ -0,0 +1,148 @@
+package options.group;
+
+import shaders.ColorblindFilter;
+import options.OptionsState;
+import flixel.addons.transition.FlxTransitionableState;
+
+class GeneralGroup
+{
+    static public function add(follow:OptionBG) {
+
+        var option:Option = new Option(
+            Language.get('General'),
+            TITLE
+        );
+        follow.addOption(option);
+       
+        var reset:ResetRect = new ResetRect(450, 20, follow);
+        follow.add(reset);
+	    
+        var option:Option = new Option(
+            Language.get('framerate'),
+            'framerate',
+            INT,
+            24,
+            500,
+	    'FPS'
+        );
+        follow.addOption(option);
+        option.onChange = onChangeFramerate;
+
+        var langArray:Array<String> = [];
+        var contents:Array<String> = FileSystem.readDirectory(Paths.getPath('language'));
+        for (item in contents) {
+            var itemPath = Paths.getPath('language') + '/' + item;
+            
+            if (FileSystem.isDirectory(itemPath)) {
+                langArray.push(item);
+            }
+        }
+        var option:Option = new Option(
+            Language.get('language'),
+            'language',
+            STRING,
+            langArray
+        );
+        follow.addOption(option);
+        option.onChange = onChangeLanguage;
+
+        var colorblindFilterArray:Array<String> = ['None', 'Protanopia', 'Protanomaly', 'Deuteranopia','Deuteranomaly','Tritanopia','Tritanomaly','Achromatopsia','Achromatomaly'];
+
+        var option:Option = new Option(
+            Language.get('colorblindMode'),
+            'colorblindMode',
+            STRING,
+            colorblindFilterArray
+        );
+        follow.addOption(option);
+        option.onChange = onChangeFilter;
+
+        var option:Option = new Option(
+            Language.get('lowQuality'),
+            'lowQuality',
+            BOOL
+        );
+        follow.addOption(option);
+
+        var option:Option = new Option(
+            Language.get('gameQuality'),
+            'gameQuality',
+            INT,
+            0,
+            3
+        );
+        follow.addOption(option);
+
+        var option:Option = new Option(
+            Language.get('antialiasing'),
+            'antialiasing',
+            BOOL
+        );
+        follow.addOption(option);
+        
+        var option:Option = new Option(
+            Language.get('flashing'),
+            'flashing',
+            BOOL
+        );
+        follow.addOption(option);
+
+        var option:Option = new Option(
+            Language.get('shaders'),
+            'shaders',
+            BOOL
+        );
+        follow.addOption(option);
+
+        var option:Option = new Option(
+            Language.get('cacheOnGPU'),
+            'cacheOnGPU',
+            BOOL
+        );
+        follow.addOption(option);
+
+        var option:Option = new Option(
+            Language.get('autoPause'),
+            'autoPause',
+            BOOL
+        );
+        follow.addOption(option);
+        option.onChange = onChangePause;
+    }
+
+    static function onChangeFramerate()
+    {
+        if(ClientPrefs.data.framerate > FlxG.drawFramerate)
+        {
+            FlxG.updateFramerate = ClientPrefs.data.framerate;
+            FlxG.drawFramerate = ClientPrefs.data.framerate;
+        }
+        else
+        {
+            FlxG.drawFramerate = ClientPrefs.data.framerate;
+            FlxG.updateFramerate = ClientPrefs.data.framerate;
+        }
+    }
+
+    static function onChangeFilter()
+    {
+        ColorblindFilter.UpdateColors();
+    }
+    
+
+    static function onChangePause()
+    {
+        FlxG.autoPause = ClientPrefs.data.autoPause;
+    }
+
+    static function onChangeLanguage()
+    {
+        Language.resetData();
+        FlxTransitionableState.skipNextTransIn = true;
+		FlxTransitionableState.skipNextTransOut = true;
+        MusicBeatState.switchState(new OptionsState());
+    }
+}
+
+
+    
